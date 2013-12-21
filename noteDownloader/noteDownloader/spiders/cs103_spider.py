44859256a7c6d98ed4711aa72f1ab6259a9e7877
C2chenn <c2.chenn@gmail.com>
2013-12-21 15:56:09 -0800

    it works now!!
    read my comments.

--- noteDownloader/noteDownloader/spiders/cs103_spider.py
+++ noteDownloader/noteDownloader/spiders/cs103_spider.py
@@ -1,11 +1,14 @@
 # This is what the cs103spider will do to get what you need.
 # add more comments later explaining what we will do.
 # also add better docstrings
-# my path syntax might not be correct. check later. 
-# handouts and discussion problems both have handouts in HTML p
-# robably should either group those or find a way to categorize the handouts.
+# This spider so far can extract the urls for the notes. 
+# IMPORTANT everythign has handout in url find a way to classify handouts specifically
+# actually do you need the hadnouts? 
 # for now i just created a list of the links (if my syntax is correct)
 # later add in how to download using pdf
+# everytime you tell the spider to crawl delete excel file. 
+# run the command scrapy crawl cs103 -o items.csv -t csv to create excel file. 
+# also there is probably a more efficient way to do this. 
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from noteDownloader.items import NotedownloaderItem
@@ -20,16 +23,32 @@ class cs103spider(BaseSpider):
 		'''Will go through everything and get the pdf urls to save into
 		a file.'''
 		sel = HtmlXPathSelector(response)
-		title = sel.select("//p")
+		category = sel.select("//p")
 		items = []
-		for obj in title:
-			item = NotedownloaderItem()
-			item["lectures"] = obj.select('a[contains(@href, "lectures")]/@href').extract() 
-			item["handouts"] = obj.select('a[contains(@href, "handouts")]/@href').extract() 
-			item["discussion_problems"] = obj.select('a[contains(@href, "Discussion Problems")]/@href').extract()
-			item["assignments"] = obj.select('a[contains(@href, "Problem Set")]/@href').extract()
-			items.append(item)
-		return items
+		for obj in category:
+			lecture = obj.select('//a[contains(@href, "lectures")]')
+			handout = obj.select('//a[contains(@href, "handouts")]')
+			discussion_problem = obj.select('//a[contains(@href, "Discussion Problems")]')
+			assignment = obj.select('//a[contains(@href, "Problem Set")]')
+
+			for l in lecture:
+				item = NotedownloaderItem()
+				item["lectures"] = l.select('@href').extract()
+				items.append(item)
+			for h in handout:
+				item = NotedownloaderItem()
+				item["handouts"] = h.select('@href').extract()
+				items.append(item)
+			for d in discussion_problem:
+				item = NotedownloaderItem()
+				item["discussion_problems"] = d.select('@href').extract()
+				items.append(item)
+			for a in assignment:
+				item = NotedownloaderItem()
+				item["assignments"] = a.select('@href').extract()
+				items.append(item)
+
+			return items
 
 
 

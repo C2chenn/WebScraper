7f8539ffca1a3ccf231c47c035fc355c39446908
Reginald Long <reggylong@gmail.com>
2013-12-23 20:22:50 -0800

    Mostly formatted pdf names

---
+++ noteDownloader/Downloads/handouts/fix_name.py
@@ -0,0 +1,14 @@
+from os import rename, listdir
+
+fnames = listdir('.')
+
+for fname in fnames:
+	result = ""
+	for char in fname:
+		if char != '%':
+			result += char
+	if result != 'fix_name.py':
+		result = result[5:]
+		if result[0] == '0':
+			result = result[0:]
+	rename(fname, result)
